4f7d9935bffab59dddc611089d1bfb2425f60221
Somnath Bhattarai <sumanbhattarai200@gmail.com>
2024-09-05 22:21:01 +0530

    added

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heartsContainer = document.querySelector(".hearts");
+
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
+  }
+
+  setInterval(createHeart, 300);
+});
